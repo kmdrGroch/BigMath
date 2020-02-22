d1cdcda542ecc79471a0bad11731daa03ac70517
kmdrGroch <leoniak.bartosz@gmail.com>
2020-02-22 13:56:15 +0100

    optimisations

--- src/comparison.ts
+++ src/comparison.ts
@@ -1,28 +1,30 @@
-import { subtract } from './basic';
 import { BigNumber } from './interfaces';
-import { normalize } from './util';
 
 /**
  * @returns If first parameter is greater than or equal second parameter
  */
-export const gte = (a: BigNumber, b: BigNumber): boolean => !subtract(a, b).sign;
+export const gte = (a: BigNumber, b: BigNumber): boolean => !lt(a, b);
 
 /**
  * @returns If first parameter is smaller than or equal second parameter
  */
-export const lte = (a: BigNumber, b: BigNumber): boolean => {
-  const k = subtract(a, b);
-
-  return k.sign || k.number === 0n;
-};
+export const lte = (a: BigNumber, b: BigNumber): boolean => !gt(a, b);
 
 /**
  * @returns If first parameter is greater than second parameter
  */
 export const gt = (a: BigNumber, b: BigNumber): boolean => {
-  const x = subtract(a, b);
+  if (a.sign !== b.sign) {
+    return b.sign;
+  }
 
-  return !x.sign && x.number !== 0n;
+  if (a.comma === b.comma) {
+    return a.sign ? a.number < b.number : a.number > b.number;
+  }
+
+  const f = a.comma > b.comma ? a.number * 10n ** BigInt(a.comma - b.comma) - b.number : a.number - b.number * 10n ** BigInt(b.comma - a.comma);
+
+  return a.sign ? f < 0n : f > 0n;
 };
 
 /**
@@ -45,19 +47,9 @@ export const lt = (a: BigNumber, b: BigNumber): boolean => {
 /**
  * @returns If first parameter is equal second parameter
  */
-export const eq = (a: BigNumber, b: BigNumber): boolean => {
-  a = normalize(a);
-  b = normalize(b);
-
-  return a.sign === b.sign && a.comma === b.comma && a.number === b.number;
-};
+export const eq = (a: BigNumber, b: BigNumber): boolean => a.sign === b.sign && a.comma === b.comma && a.number === b.number;
 
 /**
  * @returns If first parameter is not equal second parameter
  */
-export const neq = (a: BigNumber, b: BigNumber): boolean => {
-  a = normalize(a);
-  b = normalize(b);
-
-  return a.sign !== b.sign || a.comma !== b.comma || a.number !== b.number;
-};
+export const neq = (a: BigNumber, b: BigNumber): boolean => a.sign !== b.sign || a.comma !== b.comma || a.number !== b.number;
